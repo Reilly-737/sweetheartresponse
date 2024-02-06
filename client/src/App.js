1713b102e4eb379851294ec65df4d0d01afa20b7
Reilly <reillyswork@gmail.com>
2024-02-06 15:03:27 -0700

    Added client, server and README.md

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ValentineQuestion from "./components/VDayQuestion";
+
+function App() {
+  return (
+    <div className="App">
+      <ValentineQuestion />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/VDayQuestion.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ValentineQuestion = () => {
+  return (
+    <div>
+      <h1>Will You Be My Valentine?</h1>
+      <button>Yes</button>
+      <button>No</button>
+    </div>
+  );
+};
+
+export default ValentineQuestion;
